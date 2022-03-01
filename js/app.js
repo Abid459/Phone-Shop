eae0d770d4f4779fcaca32d48b62b7cb58974e77
ABID <abid45986@gmail.com>
2022-03-01 20:56:02 +0600

    details added

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 const inputName = document.querySelector('#inputName');
 const searchResultParent = document.querySelector('#search-result');
+const detailsResult = document.querySelector('.details');
 
-const search =()=>{
+const search = () => {
     const searchText = inputName.value;
-    const url= `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-     fetch(url)
-    .then(res => res.json())
-    .then(data => display(data.data))
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => display(data.data))
 }
-const display = phones =>{
-    for(let phone of phones ){
+const display = phones => {
+    for (let phone of phones) {
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `
         <div class="col">
               <div class="card">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${phone.phone_name}</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h4 class="card-title">${phone.phone_name}</h4>
+                  <p class="card-text">Release Date</p>
+                 <h4>Brand: ${phone.brand}</h4>
+                  <button onclick = "showDetails('${phone.slug}')">See details</button>
                 </div>
               </div>
             </div>
@@ -25,4 +28,28 @@ const display = phones =>{
         searchResultParent.appendChild(newDiv)
         console.log(phone)
     }
+}
+
+const showDetails = phoneId => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+        .then(res => res.json())
+        .then(details => {
+            // console.log(details);
+            displayDetails(details);
+        })
+    console.log(phoneId)
+}
+
+const displayDetails = details => {
+    const detailsDisplay = document.createElement('div');
+    const feature = details.data.mainFeatures;
+    detailsDisplay.innerHTML=`
+            <p>cheaprset: ${feature.chipSet}</p>
+              <p>Memory : ${feature.memory}</p>
+              <p>Storage:${feature.storage}</p>
+              <p>Display Size:${feature.displaySize}</p>
+              <p>Sensors<ion-icon name="caret-down-outline"></ion-icon></p>
+              <p>Others<ion-icon name="caret-down-outline"></ion-icon></p>
+    `
+    detailsResult.appendChild(detailsDisplay);
 }
