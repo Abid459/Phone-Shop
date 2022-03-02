ce299c44770bcdee48afc02c12a25d5d13462812
ABID <abid45986@gmail.com>
2022-03-02 18:45:28 +0600

    version 2.0

--- js/app.js
+++ js/app.js
@@ -8,142 +8,116 @@ const layer = document.querySelector('.layer');
 let fetchData = '';
 let range = 0;
 let count = 0;
-// let releaseDate = '';
-
+inputName.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    search();
+  }
+})
 const search = () => {
-    range = 20;
-    layer.style.display = "block";
-    spinner.style.display = "block";
-    showMore.textContent = '';
-    searchResultParent.innerHTML = '';
-    detailsResult.textContent = '';
-    detailsResult.style.display = "none";
+  range = 20;//Set the number of items to display
+  layer.style.display = "block";
+  spinner.style.display = "block";
+  showMore.textContent = '';
+  searchResultParent.innerHTML = '';
+  detailsResult.textContent = '';
+  detailsResult.style.display = "none";
 
-    const searchText = inputName.value.toLowerCase();
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    inputName.value = '';
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            fetchData = data.data;
-            console.log(data);
-            display(fetchData)
-        })
-        
-
-
+  const searchText = inputName.value.toLowerCase();
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  inputName.value = '';
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      fetchData = data.data;
+      display(fetchData)
+    })
 }
 const display = phones => {
-    if (phones.length == 0) {
-        detailsResult.style.display = "block";
-        detailsResult.innerHTML = `<h4>No phone Found.Try different name</h4>`;
-        layer.style.display = "none"
-        spinner.style.display = "none";
-        // 
-        return;
+  if (phones.length == 0) {
+    detailsResult.style.display = "block";
+    detailsResult.innerHTML = `<h4>No phone Found.Try different name</h4>`;
+    layer.style.display = "none"
+    spinner.style.display = "none";
+    return;
+  }
+  count = 0;//Reset count value
+  for (let phone of phones) {
+    if (count >= range) {
+      const showMoreBtn = document.createElement('button')
+      showMoreBtn.innerText = 'Show More';
+      showMore.appendChild(showMoreBtn);
+      showMore.addEventListener('click', () => {
+        range = 99999;//set the heighest range of items to display for Show more button
+        display(fetchData);
+        showMore.removeChild(showMoreBtn);
+
+      })
+      return;
     }
-    count = 0;
-    for (let phone of phones) {
-        // debugger;
-        // let releaseDateTemp = releaseDatefn(phone.slug);
-        // console.log(releaseDateTemp);
-        if (count >= range) {
-            const showMoreBtn = document.createElement('button')
-            showMoreBtn.innerText = 'Show More';
-            showMore.appendChild(showMoreBtn);
-            showMore.addEventListener('click', () => {
-                range = 10000;
-                display(fetchData);
-                showMore.removeChild(showMoreBtn);
-
-            })
-            // spinner.style.display = "none";
-            return;
-        }
-        count++;
-        console.log(phone);
-
-        const newDiv = document.createElement('div');
-        newDiv.innerHTML = `
+    count++;
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
         <div class="col">
               <div class="card p-3 text-center">
                 <img src="${phone.image}" class="card-img-top w-75  p-5 mx-auto" alt="...">
                 <div class="card-body">
-                  <h4 class="card-title">${phone.phone_name}</h4>
-                 <h4>Brand: ${phone.brand}</h4>
+                  <h4>Brand: ${phone.brand}</h4>
+                  <h5 class="card-title">Model: ${phone.phone_name}</h5>
                   <button onclick = "showDetails('${phone.slug}')">See details</button>
                 </div>
               </div>
             </div>
         `;
-        // setTimeout(() => {
-        // }, 2000);
-
-        searchResultParent.appendChild(newDiv)
-        spinner.style.display = "none";
-        layer.style.display = "none"
-        // console.log(phone)
-    }
+    searchResultParent.appendChild(newDiv)
+    spinner.style.display = "none";
+    layer.style.display = "none";
+  }
 }
 
 const showDetails = phoneId => {
-    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
-        .then(res => res.json())
-        .then(details => {
-            // console.log(details.data);
-            displayDetails(details);
-        })
-    // console.log(phoneId)
+  fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+    .then(res => res.json())
+    .then(details => {
+      displayDetails(details);
+    })
 }
 
 const displayDetails = details => {
-  console.log(details)
-    detailsResult.textContent = '';
-    detailsResult.style.display = "block";
-    const detailsDisplay = document.createElement('div');
-    const feature = details.data.mainFeatures;
-    const sensors = feature.sensors;
-    const otherFeature = details.data?.others ? Object.entries(details.data.others):"No additional feature";
-    const releaseDate =details.data?.releaseDate ? details.data.releaseDate:`<span style = "color:#C77149">No releasedate found</span>`;
-    detailsDisplay.innerHTML = `
-    <img src = "${details.data.image}">
+  detailsResult.textContent = '';
+  detailsResult.style.display = "block";
+  const detailsDisplay = document.createElement('div');
+  const feature = details.data.mainFeatures;
+  const sensors = feature.sensors;
+
+  const releaseDate = details.data?.releaseDate ? details.data.releaseDate : `<span style = "color:#C77149">No release date found</span>`;// Release date opitonal chaining
+  detailsDisplay.innerHTML = `
+            <img src = "${details.data.image}">
             <p><b>Brand:</b> ${details.data.brand}</p>
             <p><b>Model:</b> ${details.data.name}</p>
             <p><b>Release date:</b> ${releaseDate}</p>
-            <p><b>cheaprset:</b> ${feature.chipSet}</p>
-              <p><b>Memory:</b> ${feature.memory}</p>
-              <p><b>Storage:</b> ${feature.storage}</p>
-              <p><b>Display Size:</b> ${feature.displaySize}</p>
-
-              <div class="accordion" id="accordionExample">
-              <div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Sensors
-                  </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
-                    ${sensors}
-                  </div>
-                </div>
-              </div>
+            <p><b>Cheaprset:</b> ${feature.chipSet}</p>
+            <p><b>Memory:</b> ${feature.memory}</p>
+            <p><b>Storage:</b> ${feature.storage}</p>
+            <p><b>Display Size:</b> ${feature.displaySize}</p>
             
+            <!-- sensors------- -->
+            <p class = "sensors"><b>Sensors: </b>${sensors}</p>
 
-            <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Others
-              </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-              ${otherFeature}
-              </div>
-            </div>
-          </div>
-          </div>
-    `
-    detailsResult.appendChild(detailsDisplay);
+            <!-- other feature------- -->
+            <div class="other-feature"></div>
+    `;
+  detailsResult.appendChild(detailsDisplay);
+  // Other feature conditional append
+if(details.data?.others){
+  const otherFeaturesParent = document.querySelector('.other-feature');
+  const otherFeatures = details.data?.others;
+  for(let otherFeature in otherFeatures){
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+    <P><b>${otherFeature}:</b> ${otherFeatures[otherFeature]} </p> 
+    `;
+    otherFeaturesParent.appendChild(newDiv);
+  }
+}
 }
 
